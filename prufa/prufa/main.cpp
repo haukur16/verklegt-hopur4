f80ae3e31f8a44c39788561a3e8e57fdc8f5fc06
haukur <haukur16@ru.is>
2017-11-28 12:51:55 +0000

    tekka

---
+++ prufa/prufa/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  prufa
+//
+//  Created by Haukur Guðmundsson on 28/11/2017.
+//  Copyright © 2017 Haukur Guðmundsson. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Enter a number: " << endl;
+    int number;
+    cin >> number;
+    cout << number+number;
+    return 0;
+}
